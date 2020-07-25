cab1fdc93f3b9124861552753f0370b298ecd0a9
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-07-25 21:04:59 +0530

    removed unnecessary comments from cache

--- cache/lrucache.go
+++ cache/lrucache.go
@@ -30,11 +30,11 @@ func New(cap int) *LRUCache {
 
 //Get ... get the element value via key if it exists
 func (cache *LRUCache) Get(location string) *models.Places {
-	//if key exist in map
+
 	if node, ok := cache.elementMap[location]; ok {
-		// fetch vale
+
 		val := node.Value.(*list.Element).Value.(PlaceInfoCache).POIData
-		//move itemto front in list
+
 		cache.list.MoveToFront(node)
 		return &val
 	}
@@ -44,32 +44,32 @@ func (cache *LRUCache) Get(location string) *models.Places {
 //Put ... put item in list and map to a hashmap if not exist
 //if exist move it to front
 func (cache *LRUCache) Put(location string, poiPlaces models.Places) {
-	//if value exist 1. update the value 2. move it to front
+
 	if node, ok := cache.elementMap[location]; ok {
-		//update the value in list node
+
 		node.Value.(*list.Element).Value = poiPlaces
-		//move it to the first node
+
 		cache.list.MoveToFront(node)
 	} else {
-		//check size and delete the last one
+
 		if cache.list.Len() == cache.capacity {
-			//get the last key
+
 			lastLocationInCache := cache.list.Back().Value.(*list.Element).Value.(PlaceInfoCache).Location
-			//delete this key from element map
+
 			delete(cache.elementMap, lastLocationInCache)
-			//remove it from cache list
+
 			cache.list.Remove(cache.list.Back())
 		}
-		//create a new node
+
 		newNode := &list.Element{
 			Value: PlaceInfoCache{
 				Location: location,
 				POIData:  poiPlaces,
 			},
 		}
-		//insert this node in front of list
+
 		nodePtr := cache.list.PushFront(newNode)
-		//update map index for fast lookup
+
 		cache.elementMap[location] = nodePtr
 	}
 }
