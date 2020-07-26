df5ac96557d3234f069bd8c1f06fec1cc458de9b
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-07-26 13:12:20 +0530

    unit test for getting poi for all 3 categories

--- httpclient/here-http_test.go
+++ httpclient/here-http_test.go
@@ -33,10 +33,9 @@ func TestGetLocationCoordinates(t *testing.T) {
 		//when request is correct
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("{\"items\":[{\"position\":{\"lat\":52.53041,\"lng\":13.38527}}]}"))
-		return
 	}
 	testServer := httptest.NewServer(http.HandlerFunc(handler))
-	testServerURL := testServer.URL
+	defer testServer.Close()
 
 	tests := []struct {
 		name             string
@@ -48,7 +47,7 @@ func TestGetLocationCoordinates(t *testing.T) {
 	}{
 		{
 			name:             "it should return error, when query params are not present",
-			hereGecodeAPIURL: testServerURL + "?qu=%s&apiKey=%s",
+			hereGecodeAPIURL: testServer.URL + "?qu=%s&apiKey=%s",
 			hereAPIKey:       "",
 			locationName:     "London",
 			want:             models.Position{},
@@ -56,7 +55,7 @@ func TestGetLocationCoordinates(t *testing.T) {
 		},
 		{
 			name:             "it should return position, on correct request url",
-			hereGecodeAPIURL: testServerURL + "?q=%s&apiKey=%s",
+			hereGecodeAPIURL: testServer.URL + "?q=%s&apiKey=%s",
 			hereAPIKey:       "hereSecret",
 			locationName:     "London",
 			want:             models.Position{Lat: 52.53041, Lng: 13.38527},
@@ -80,24 +79,65 @@ func TestGetLocationCoordinates(t *testing.T) {
 			}
 		})
 	}
-	testServer.Close()
 }
 
 func TestGetPOIsNearALocation(t *testing.T) {
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		log.Println("*** test server invoked ****")
+		//validate http method
+		if r.Method != http.MethodGet {
+			t.Errorf("want http method %s, got %s", http.MethodGet, r.Method)
+			http.Error(w, "http method should be GET", http.StatusInternalServerError)
+			return
+		}
+		//validate http query params
+		queryParams := r.URL.Query()
+		category := queryParams.Get("categories")
+		switch category {
+		case categoriesOfPOI[restaurent], categoriesOfPOI[evChargingStation], categoriesOfPOI[parking]:
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("{\"items\":[{\"position\":{\"lat\":52.53041,\"lng\":13.38527}}]}"))
+		}
+	}
+	testServer := httptest.NewServer(http.HandlerFunc(handler))
+	defer testServer.Close()
+
 	type args struct {
 		poiPosition models.Position
 	}
 	tests := []struct {
-		name    string
-		args    args
-		want    *models.Places
-		wantErr bool
+		name             string
+		hereBrowseAPIURL string
+		position         models.Position
+		want             *models.Places
+		wantErr          bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:             "it should return data for all three category",
+			hereBrowseAPIURL: testServer.URL + "?at=%s3&limit=3&categories=%s&apiKey=%s",
+			position:         models.Position{Lat: 123, Lng: 235},
+			want: &models.Places{
+				Restaurents: []models.PlaceInfo{
+					{Position: models.Position{Lat: 52.53041, Lng: 13.38527}},
+				},
+				EvChargingStations: []models.PlaceInfo{
+					{Position: models.Position{Lat: 52.53041, Lng: 13.38527}},
+				},
+				ParkingLots: []models.PlaceInfo{
+					{Position: models.Position{Lat: 52.53041, Lng: 13.38527}},
+				},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetPOIsNearALocation(tt.args.poiPosition)
+
+			hereBrowseAPIURL = tt.hereBrowseAPIURL
+
+			got, err := GetPOIsNearALocation(tt.position)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetPOIsNearALocation() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -105,23 +145,6 @@ func TestGetPOIsNearALocation(t *testing.T) {
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetPOIsNearALocation() = %v, want %v", got, tt.want)
 			}
-		})
-	}
-}
-
-func Test_getNearByPlaceForACategory(t *testing.T) {
-	type args struct {
-		poi poiMetaData
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			getNearByPlaceForACategory(tt.args.poi)
 		})
 	}
 }
